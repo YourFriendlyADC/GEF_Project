a3594c50ed470c497ba24ce07779dd7436b1a18a
Alejandro <jhalejandro@uninorte.edu.co>
2022-09-18 22:00:28 -0500

    matrices y modelo de ventana de usuario

--- GEF_Project/src/game/Sketch.java
+++ GEF_Project/src/game/Sketch.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.util.Scanner;
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -13,134 +12,87 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
-        Scanner sc = new Scanner(System.in);
-        // Temperatura, Frecuencia, Amplitud de Desplazamiento, Densidad.
-        int temp, frec, ampd_desp, densidad;
-        String usuario[] = new String[5];
-        for (int i = 0; i < 5; i++) {
-                switch (i) {
-                    case 0: // Nombre de Usuario.
-                        System.out.print("Digite su nombre: ");
-                        usuario[i] = sc.next();
-                        break;
-                    case 1:
-                        System.out.print("Digite la temperatura (10-37) Grados Celsius: ");
-                        usuario[i] = Integer.toString(temp = sc.nextInt());
-                        while (temp < 10 || temp > 37) {
-                            System.out.println("La temperatura debe ser entre 10 y 37 grados: ");
-                            System.out.print("Digitela de nuevo: ");
-                            usuario[i] = Integer.toString(temp = sc.nextInt());
-                        }
-                        break;
-                    case 2:
-                        System.out.print("Digite la frecuencia (20-20000)Hz: ");
-                        usuario[i] = Integer.toString(frec = sc.nextInt());
-                        while (frec < 20 || frec > 20000) {
-                            System.out.println("La frecuencia debe ser entre 20Hz y 20000Hz: ");
-                            System.out.print("Digitela de nuevo: ");
-                            usuario[i] = Integer.toString(frec = sc.nextInt());
-                        }
-                        break;
-                    case 3:
-                        System.out.print("Digite la Amplitud de Desplazamiento: ");
-                        usuario[i] = Integer.toString(ampd_desp = sc.nextInt());                       
-                        break;
-                    case 4:
-                        System.out.println("Densidad");
-                        break;
-                }            
-            System.out.println("");
-        }
-        for (int i = 0; i < 5; i++) {
-                System.out.println(usuario[i]);
-            }
-            System.out.println("");
+//        Scanner sc = new Scanner(System.in);
+//        // Temperatura, Frecuencia, Amplitud de Desplazamiento, Densidad.
+//        int temp, frec, ampd_desp, densidad;
+//        int n;
+//        // Nombre de Usuario.
+//        String usuario[][] = new String[5][100];
+//        System.out.print("Numero de usuarios: ");
+//        n = sc.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                switch (j) {
+//                    case 0:
+//                        System.out.print("Digite su nombre: ");
+//                        usuario[i][j] = sc.next();
+//                        break;
+//                    case 1:
+//                        System.out.print("Digite la temperatura (10-37) Grados Celsius: ");
+//                        usuario[i][j] = Integer.toString(temp = sc.nextInt());
+//                        while (temp < 10 || temp > 37) {
+//                            System.out.println("La temperatura debe ser entre 10 y 37 grados: ");
+//                            System.out.print("Digitela de nuevo: ");
+//                            usuario[i][j] = Integer.toString(temp = sc.nextInt());
+//                        }
+//                        break;
+//                    case 2:
+//                        System.out.print("Digite la frecuencia (20-20000)Hz: ");
+//                        usuario[i][j] = Integer.toString(frec = sc.nextInt());
+//                        while (frec < 20 || frec > 20000) {
+//                            System.out.println("La frecuencia debe ser entre 20Hz y 20000Hz: ");
+//                            System.out.print("Digitela de nuevo: ");
+//                            usuario[i][j] = Integer.toString(frec = sc.nextInt());
+//                        }
+//                        break;
+//                    case 3:
+//                        System.out.print("Digite la Amplitud de Desplazamiento: ");
+//                        usuario[i][j] = Integer.toString(ampd_desp = sc.nextInt());                       
+//                        break;
+//                    case 4:
+//                        System.out.println("Densidad");
+//                        System.out.println("");
+//                        break;
+//                }
+//            }            
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                System.out.println(usuario[i][j]);
+//            }
+//            System.out.println("");
+//        }
     }
 
-    int red_darker_rectX = 100, red_mid_recY = 195, red_lighter_rectY = 335, red_mid_recX = 100;
-    int green_darker_rectX = 270, green_lighter_rectX = 270, green_darker_rectY = 45, green_lighter_rectY = 335;
-    int blue_darker_rectY = 45, blue_mid_rectY = 195, blue_darker_rectX = 440, blue_mid_rectX = 440;
-    int grados = 0;
     @Override
     public void draw() {
-        PImage img;
-        img = loadImage("images/background.png");
-        background(img);
-//        background(238, 195, 255);        
+        PImage bgd, ear;
+        bgd = loadImage("images/bgd.png");
+        ear = loadImage("images/ear.png");
+        background(bgd);
+
+        fill(0);
+//        line(0, 240, 640, 240);
+//        line(320, 0, 320, 480);
+
+        fill(179, 95, 44);
+        // Lineas
+        line(0, 10, 630, 10);
+        line(630, 0, 630, 470);
+        line(640, 470, 10, 470);
+        line(10, 480, 10, 10);
+        // Titulo
+        rect(195, 40, 250, 60);
+        // Icono
+        rect(153, 163, 153, 153);
+        // "Nombre"
+        rect(360, 200, 140, 25);
+        // Nombre
+        rect(340, 240, 180, 40);
+
+        fill(255);
+        image(ear, 155, 165, 150, 150);
         
-        fill(185, 19, 44); // Red rectangles
-        rect(red_darker_rectX, 45, 100, 100); // Top
-        fill(237, 82, 105);
-        rect(red_mid_recX, red_mid_recY, 100, 100); // Middle
-        fill(277, 162, 185);
-        rect(100, red_lighter_rectY, 100, 100); // Bottom
-        red_darker_rectX++; // first rectangle change of position
-        if (red_darker_rectX == 440) {
-            red_darker_rectX--;
-        }
-        red_mid_recY--;
-        if (red_mid_recY == 45) {
-            red_mid_recY++;
-            red_mid_recX++;
-            if (red_mid_recX == 270) {
-                red_mid_recX--;
-            }
-        }
-        red_lighter_rectY--;
-        if (red_lighter_rectY == 45) {
-            red_lighter_rectY++;
-        }
-
-        fill(58, 146, 94); // Green rectangles
-        rect(green_darker_rectX, green_darker_rectY, 100, 100); // Top
-        fill(119, 200, 151);
-        rect(270, 195, 100, 100); // Middle
-        fill(159, 243, 191);
-        rect(green_lighter_rectX, green_lighter_rectY, 100, 100); // Bottom
-        green_darker_rectX++;
-        if (green_darker_rectX == 440) {
-            green_darker_rectX--;
-            green_darker_rectY++;
-            if (green_darker_rectY == 195) {
-                green_darker_rectY--;
-            }
-        }
-        green_lighter_rectX--;
-        if (green_lighter_rectX == 100) {
-            green_lighter_rectX++;
-            green_lighter_rectY--;
-            if (green_lighter_rectY == 195) {
-                green_lighter_rectY++;
-            }
-        }      
-
-        fill(11, 60, 191); // Blue rectangles
-        rect(440, blue_darker_rectY, 100, 100); // Top
-        fill(72, 119, 244);
-        rect(blue_mid_rectX, blue_mid_rectY, 100, 100); // Middle
-        fill(142, 169, 284);
-        rect(blue_darker_rectX, 335, 100, 100); // Bottom
-        blue_darker_rectY++;
-        if (blue_darker_rectY == 335) {
-            blue_darker_rectY--;
-        }
-        blue_mid_rectY++;
-        if (blue_mid_rectY == 335) {
-            blue_mid_rectY--;
-            blue_mid_rectX--;
-            if (blue_mid_rectX == 270) {
-                blue_mid_rectX++;
-            }
-        }
-        blue_darker_rectX--;
-        if (blue_darker_rectX == 100) {
-            blue_darker_rectX++;
-        }
-
-//        line(100, 45, 100, 435); // Left
-//        line(540, 45, 540, 435); // Right
-//        line(100, 45, 540, 45); // Top
-//        line(100, 435, 540, 435); // Bottom
     }
 
     public void run() {
